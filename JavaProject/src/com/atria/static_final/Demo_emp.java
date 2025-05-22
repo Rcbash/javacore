6beef79606782e771cccd339810d93b912f42587
Rcbash <160474152+Rcbash@users.noreply.github.com>
2025-05-22 13:42:08 +0530

    Add files via upload

---
+++ JavaProject/src/com/atria/static_final/Demo_emp.java
@@ -0,0 +1,13 @@
+package com.atria.static_final;
+
+public class Demo_emp {
+
+	public static void main(String[] args) {
+		Emploice mustwashans=new Emploice("Ashwin",123);
+		System.out.println(mustwashans);
+		Emploice e2=new Emploice("Suraj",234);
+		System.out.println(e2);
+
+	}
+
+}
